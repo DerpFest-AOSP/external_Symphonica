4ca66c42ee7faaf43e01c53d04651b2848e8fd3b
NurKeinNeid <mralexman3000@gmail.com>
2023-07-23 13:48:42 +0200

    fixup! Initialize Symphonica from `0.5.9.indev.4`

--- Android.mk
+++ Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE := Symphonica
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := prebuilt/Symphonica.apk
+LOCAL_SRC_FILES := prebuilt/symphonica_signed.apk
 
 include $(BUILD_PREBUILT)
